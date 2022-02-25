08dc728ce631c1e5382ecc72428f6737d7c1e0a1
Ankit Agarwal <ankit@applied.co>
2022-02-24 19:27:40 -0800

    updated nanopb generator to handle enums better

--- generator/nanopb_generator.py
+++ generator/nanopb_generator.py
@@ -353,7 +353,7 @@ class Enum(ProtoElement):
         self.names = names
 
         # by definition, `names` include this enum's name
-        base_name = Names(names.parts[:-1])
+        base_name = Names(names.parts[-1])
 
         if enum_options.long_names:
             self.values = [(names + x.name, x.number) for x in desc.value]
